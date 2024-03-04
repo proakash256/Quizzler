3c5920cced7b17aefbc2051ed28d271cfac58be1
Akash <proakash256@gmail.com>
2024-03-04 22:48:17 +0530

    Initial Commit

---
+++ data.py
@@ -0,0 +1,6 @@
+import requests
+
+parameters = {"amount": 10, "category": 18, "type": "boolean"}
+response = requests.get("https://opentdb.com/api.php", params=parameters)
+response.raise_for_status()
+question_data = response.json()["results"]
